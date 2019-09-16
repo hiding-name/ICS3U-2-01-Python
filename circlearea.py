ef1b3804430e4927a51bb4382194d018d98d46b0
Ubuntu <ubuntu@ip-172-31-18-91.ec2.internal>
2019-09-16 21:14:18 +0000

    Initial and only code

---
+++ circlearea.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+# Created by: Matsuru Hoshi
+# Created on: Sept 2019
+# This is program area preiormte rect
+
+import math
+
+
+def main():
+    print("The radius of the circle is 15mm \n")
+    print("The area is: " + format((math.pi * 15) ** 2))
+    print("The circumference is: " + format(2 * math.pi * 15))
+
+
+if __name__ == "__main__":
+    main()
